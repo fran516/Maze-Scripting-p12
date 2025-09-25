9633b5ebb4599f2ac655f903b285d2b2c7182bcc
fran516 <rzq512@york.ac.uk>
2025-09-25 17:31:10 +0100

    25/9/25

--- Assets/MazeMovement.cs
+++ Assets/MazeMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MazeMovement : MonoBehaviour
 {
+    float xAxis = 0.0f;
+    float zAxis = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,8 @@ public class MazeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    xAxis = Input.GetAxis("Vertical") * 10;
+    zAxis = Input.GetAxis("Horizontal") * 10;
+    transform.rotation = Quaternion.Euler(xAxis, 0.0f, -zAxis);    
     }
 }
